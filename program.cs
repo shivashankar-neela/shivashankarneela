f5da0de5e4762d901a1fd1c8ef36cd9640eb6187
shivashankar-neela <84958768+shivashankar-neela@users.noreply.github.com>
2022-06-16 10:04:52 +0530

    Update program.cs

--- program.cs
+++ program.cs
@@ -1,1 +1,29 @@
-#csharp programs
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgFundamentals1             //DO NOT CHANGE the name of namespace
+{
+    public class Program                //DO NOT CHANGE the name of class 'Program'
+    { 
+        public static void Main(string[] args)        //DO NOT CHANGE 'Main' Signature
+        {
+           
+          Console.WriteLine("Enter your name:");
+            string userName = Console.ReadLine();
+             Console.WriteLine("Enter your age:");
+            int userAge=Convert.ToInt16(Console.ReadLine()); 
+              Console.WriteLine("Enter your country:");
+              string userCountry = Console.ReadLine();
+              Console.WriteLine("Welcome "+ userName+" Your age is " +userAge  +" and you are from " +userCountry);
+              
+             
+             
+            
+          
+    
+        }
+    }
+}
